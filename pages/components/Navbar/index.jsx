e07029613f2635f6450e08279a1de404007852ec
romerocam <romerocam@hotmail.com>
2023-01-11 12:48:57 -0300

    creating navbar

---
+++ pages/components/Navbar/index.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Navbar = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [characters, setCharacters] = useState([]);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(
+        `https://swapi.dev/api/people/?search=${searchValue}`
+      );
+      setCharacters(response.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="bg-gray-800 p-3 flex justify-center">
+      <form onSubmit={handleSearch} className="bg-white rounded-lg p-2 flex">
+        <input
+          className="border p-1 rounded-lg"
+          type="text"
+          placeholder="Search characters..."
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <button
+          className="bg-blue-500 text-white rounded-lg p-1 ml-2"
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+      <div className="flex flex-wrap justify-center">
+        {characters.map((character) => (
+          <div
+            key={character.name}
+            className="bg-white p-3 m-3 rounded-lg max-w-sm"
+          >
+            <h2 className="text-lg font-medium">{character.name}</h2>
+            <p className="text-sm">Birth year: {character.birth_year}</p>
+            <p className="text-sm">Gender: {character.gender}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
@@ -9,9 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+      <Navbar />
     </>
   );
 }
