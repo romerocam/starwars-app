f1c76f1afb0bfa699e65d891a5e44d55b964f73c
romerocam <romerocam@hotmail.com>
2023-01-18 11:13:58 -0300

    setting characters component

--- components/Characters/index.jsx
+++ components/Characters/index.jsx
@@ -7,10 +7,10 @@ const Characters = ({ props }) => {
   const [characters, setCharacters] = useState(props.results);
   const [next, setNext] = useState(props.next);
   const [previous, setPrevious] = useState(props.previous);
-  console.log("PROPS", props);
-  console.log("NEXT", next);
-  console.log("PREVIOUS", previous);
-  console.log("CHARACTERS", characters);
+  // console.log("PROPS", props);
+  // console.log("NEXT", next);
+  // console.log("PREVIOUS", previous);
+  // console.log("CHARACTERS", characters);
 
   const handleNextPage = () => {
     axios.get(next).then((response) => {
