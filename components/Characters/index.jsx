dba74c02f29b38a5075b7c6f0ace79cd5cbca709
romerocam <romerocam@hotmail.com>
2023-01-20 11:07:23 -0300

    testing navbar

--- components/Characters/index.jsx
+++ components/Characters/index.jsx
@@ -31,7 +31,7 @@ const Characters = ({ props }) => {
   useEffect(() => {
     axios.get("https://swapi.dev/api/people/").then((res) => {
       setCharacters(res.data);
-      // console.log("CHARACTERS IN USEEFFECT--->", characters);
+      console.log("CHARACTERS IN USEEFFECT--->", characters);
       setNext(res.data.next);
       // console.log("NEXT in USEEFFECT--->", props.next);
     });
